725339b9280315d0617a8b026965e556c4fc816e
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2025-03-13 16:03:17 +0700

    Create in-dau-sao-tuong-ung-so.cpp

---
+++ in-dau-sao-tuong-ung-so.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+    using namespace std;
+        int main(){
+        int  n;
+        cin>>n;
+            for (int i = 1; i <= n; i++ ){
+                for (int a = 1; a<=i; a++){
+                    cout<<"*";                }
+                    cout<<endl;
+            }
+            return 0;
+        }
