b59c59897ebdb695a7d559a4006e9634c3534f6e
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2025-03-26 21:47:04 +0700

    Update in-dau-sao-tuong-ung-so.cpp

--- in-dau-sao-tuong-ung-so.cpp
+++ in-dau-sao-tuong-ung-so.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
     using namespace std;
         int main(){
-        int  n;
-        cin>>n;
-            for (int i = 1; i <= n; i++ ){
-                for (int a = 1; a<=i; a++){
-                    cout<<"*";                }
-                    cout<<endl;
+            int sao;
+                cin>>sao;
+                    for (int i = 1; i <= sao; i++){
+                        for (int j = 1; j <= i; j++ ){
+                            cout<<"*";
+
+                            }
+                            cout<<endl;
+                        }
+                    for (int s = sao - 1 ; s >=1; s--){
+
+                        cout << string(sao - s, ' '); // In khoảng trắng để căn giữa
+
+                        cout << string(2 * s - 1, '*') << endl; // In số lượng sao giảm dần
+
+                    }
+
+                    return 0;
             }
-            return 0;
-        }
