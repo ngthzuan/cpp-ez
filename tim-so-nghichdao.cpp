8e544be17d4927d2ac5b2df3eefef81a5bc7f07d
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2025-03-27 14:58:32 +0700

    Update tim-so-nghichdao.cpp

--- tim-so-nghichdao.cpp
+++ tim-so-nghichdao.cpp
@@ -1,19 +1,30 @@
 #include <bits/stdc++.h>
     using namespace std;
         int main(){
-            int n;
+            /*int n;
                 cin>>n;
                   int x = 0;
                   int d = 0;
                   int t = 0;
-                 
+
                     while (n != 0){
                         t += n % 10;
                         d++;
                         x = x * 10 + n % 10;
                         n =n/10;
                     }
-                    cout<<t<<" \n"<<x<<" \n"<<d;
+                    cout<<x<<" \n"<<t<<" \n"<<d;
+                    return 0;*/
+            string n;
+                cin>>n;
+                string x = "";
+                int t = 0;
+                    int pt = n.length();
+                    for (int i = pt - 1; i >= 0; i--){
+                        x = x + n[i];
+                            t=t+(n[i]-'0');
+                    }
+                    cout<<x<<"\n"<<t<<"\n"<<pt;
                     return 0;
 
         }
