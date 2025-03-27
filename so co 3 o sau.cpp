5af3e6f0d0b9d48feaceaaad9990da4c2f739dc4
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2025-03-27 16:26:49 +0700

    Create so co 3 o sau.cpp

---
+++ so co 3 o sau.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+    using namespace std;
+        int main(){
+            int a;
+            int t = 0;
+           cin>>a;
+            for (int i = 1; i <= a; i++){
+                t+=(i*10)+3;
+                cout<<t<<" ";
+            }
+
+        }
