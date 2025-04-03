18ba8ca72733de663384464e2f169f8e35901c29
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2025-04-03 16:28:40 +0700

    Create tong-chu-so.cpp

---
+++ tong-chu-so.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+    using namespace std;
+        int main(){
+            freopen("BAI30.INP","r",stdin);
+            freopen("BAI30.OUT","w",stdout);
+            string n;
+            long long t=0;
+                cin>>n;
+            for (int i = 0; i < n.length(); i++){
+                t+=(n[i]-'0');
+
+            }
+cout<<t;            return 0;
+
+        }
+
