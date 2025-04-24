cf1efd91f93c8a2f702d5217b5d127c9c1f372dd
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2025-04-24 14:26:50 +0700

    Update nhiphan.cpp

--- nhiphan.cpp
+++ nhiphan.cpp
@@ -1,10 +1,9 @@
-#include<algorithm>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
-int test(vector<int> m, int n, int k) {
-	int l = 0,r = n - 1;
-	while (l <= r) {
-		int a = (l + r) / 2;
+int np (vector<int> m, int n, int k) {
+	int l=0,r=n-1;
+	while (l<=r) {
+		int a = (l+r) / 2;
 		if (m[a] == k)return 1;
 		else if (k > m[a])l = a + 1;
 		else r = a - 1;
@@ -12,12 +11,13 @@ int test(vector<int> m, int n, int k) {
 	return -1;
 }
 int main() {
+    freopen("bai37.inp","r", stdin);
 	int t; cin >> t;
 	while (t--) {
 		int n, k; cin >> n >> k;
 		vector<int> m(n);
 		for (int i = 0; i < n; i++)cin >> m[i];
 		sort(m.begin(), m.end());
-		cout << test(m, n, k) << endl;
+		cout << np(m, n, k) << endl;
 	}
 }
