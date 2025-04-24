51bd687f6322a408f1f8a914d6f7e78be51d701f
ngthzuan <157598689+ngthzuan@users.noreply.github.com>
2025-04-24 14:51:12 +0700

    Create so thoaman.cpp

---
+++ so thoaman.cpp
@@ -0,0 +1,22 @@
+//bai 4 de huyen
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k,d=0;
+    cin>>n>>k;
+    int a[n];
+    for (int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    for (int i=1;i<=n;i++){
+        for (int j=i;j<=n;j++){
+            if (a[i]+a[j]==k){
+                cout<<i<<" "<<j<<endl;
+              d++;}
+        }
+    }
+    cout<<d;
+    return 0;
+}
